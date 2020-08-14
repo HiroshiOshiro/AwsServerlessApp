5689d3498e166651d8929c0bd6a0ef48aa36e6eb
hiroshi oshiro <ooshiro010@gmail.com>
2020-08-14 23:17:42 +0900

    first commit
    done till 96. Adding Signup to the Frontend App

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs/Subject';
+
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+
+import { User } from './user.model';
+
+const POOL_DATA = {
+  UserPoolId: 'ap-northeast-1_esCoRf3dQ',
+  ClientId: '7tigr6lsh8c0bl93ioehgijppb'
+}
+const userPool = new CognitoUserPool(POOL_DATA);
+
+@Injectable()
+export class AuthService {
+  authIsLoading = new BehaviorSubject<boolean>(false);
+  authDidFail = new BehaviorSubject<boolean>(false);
+  authStatusChanged = new Subject<boolean>();
+  registerdUser: CognitoUser;
+
+  constructor(private router: Router) {}
+  signUp(username: string, email: string, password: string): void {
+    this.authIsLoading.next(true);
+    const user: User = {
+      username: username,
+      email: email,
+      password: password
+    };
+    const attrList: CognitoUserAttribute[] = [];
+    const emailAttribute = {
+      Name: 'email',
+      Value: user.email
+    };
+    attrList.push(new CognitoUserAttribute(emailAttribute));
+    userPool.signUp(user.username, user.password, attrList, null, (err, result) => {
+      if (err) {
+        this.authDidFail.next(true);
+        this.authIsLoading.next(false);
+        return;
+      }
+      this.authDidFail.next(false);
+      this.authIsLoading.next(false);
+      this.registerdUser = result.user;
+    });
+    return;
+  }
+  confirmUser(username: string, code: string) {
+    this.authIsLoading.next(true);
+    const userData = {
+      Username: username,
+    };
+  }
+  signIn(username: string, password: string): void {
+    this.authIsLoading.next(true);
+    const authData = {
+      Username: username,
+      Password: password
+    };
+    this.authStatusChanged.next(true);
+    return;
+  }
+  getAuthenticatedUser() {
+  }
+  logout() {
+    this.authStatusChanged.next(false);
+  }
+  isAuthenticated(): Observable<boolean> {
+    const user = this.getAuthenticatedUser();
+    const obs = Observable.create((observer) => {
+      if (!user) {
+        observer.next(false);
+      } else {
+        observer.next(false);
+      }
+      observer.complete();
+    });
+    return obs;
+  }
+  initAuth() {
+    this.isAuthenticated().subscribe(
+      (auth) => this.authStatusChanged.next(auth)
+    );
+  }
+}
